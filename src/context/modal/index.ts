c0617b2ed0ac1d977972d8625f0836dd567ecb80
Zabashtevich <vlados.zabashta@yandex.ru>
2021-06-30 14:45:14 +0200

    fix: change tsconfig and small fixes ruinning build

--- src/context/modal/index.ts
+++ src/context/modal/index.ts
@@ -1,6 +1,6 @@
-import { ModalContext } from "./context";
+import { IModalContext, ModalContext } from "./context";
 import { useContext } from "react";
 
-export default function useModalContext() {
+export default function useModalContext(): IModalContext {
   return useContext(ModalContext);
 }
