bde751d8078adfc97392696482db5b7a1181f928
Zabashtevich <vlados.zabashta@yandex.ru>
2021-06-30 16:27:20 +0200

    add: svg loader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+  ebpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
   reactStrictMode: true,
   env: {
     API_URL: "https://rickandmortyapi.com/graphql",
